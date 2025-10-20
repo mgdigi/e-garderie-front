8cb2e1dc85615ddeb5b66aae59ffed09fc932348
mgdigi <gueyeprophete287@gmail.com>
2025-10-20 15:39:36 +0000

    changement aussi du footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
             <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
               <p className="text-sm text-gray-800">
-                © 2025 e-gardrie  Tous droits réservés.
+                © 2025  Tous droits réservés.
               </p>
               
             </div>
